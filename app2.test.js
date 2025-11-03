ebe417afaa870d239ef1aa04621a68ba949aea0a
thomassio2 <thomasmuller3040@gmail.com>
2025-11-03 17:47:04 +0100

    add fonction test

--- app2.test.js
+++ app2.test.js
@@ -1,2 +1,11 @@
 const Cube = require('../src/app').Cube;
 const expect = require('chai').expect;
+
+describe('Testing the Cube Functions', function() {
+    it('1. The side length of the Cube', function(done) {
+        let c1 = new Cube(2);
+        expect(c1.getSideLength()).to.equal(2);
+        done();
+    });
+
+});
